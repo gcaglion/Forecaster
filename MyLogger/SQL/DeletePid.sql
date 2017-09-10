d4094b49c99232ef84a3177d944d2cdea55b1e75
gcaglion <gcaglion@yahoo.com>
2017-09-10 12:12:08 +0200

    minor changes to excel and DeletePid.sql

--- MyLogger/SQL/DeletePid.sql
+++ MyLogger/SQL/DeletePid.sql
@@ -1,22 +1,22 @@
 set verify off
-define pid=43988
-delete from COREIMAGE_NN where processid=&&pid;
-delete from COREIMAGE_SOM where processid=&&pid;
-delete from COREIMAGE_SVM where processid=&&pid;
-delete from CORELOGS_GA where processid=&&pid;
-delete from CORELOGS_NN where processid=&&pid;
-delete from CORELOGS_NN_SCGD where processid=&&pid;
-delete from CORELOGS_SOM where processid=&&pid;
-delete from CORELOGS_SVM where processid=&&pid;
-delete from COREPARMS_GA where processid=&&pid;
-delete from COREPARMS_NN where processid=&&pid;
-delete from COREPARMS_SOM where processid=&&pid;
-delete from COREPARMS_SVM where processid=&&pid;
-delete from MYLOG_MSE where processid=&&pid;
-delete from MYLOG_RUN where processid=&&pid;
-delete from ENGINETHREADS where processid=&&pid;
-delete from DATAPARMS where processid=&&pid;
-delete from ENGINEPARMS where processid=&&pid;
-delete from TESTERPARMS where processid=&&pid;
+define pid=896376,893432,896064,895104,885572,888092,866372,877912
+delete from COREIMAGE_NN where processid in(&&pid);
+delete from COREIMAGE_SOM where processid in(&&pid);
+delete from COREIMAGE_SVM where processid in(&&pid);
+delete from CORELOGS_GA where processid in(&&pid);
+delete from CORELOGS_NN where processid in(&&pid);
+delete from CORELOGS_NN_SCGD where processid in(&&pid);
+delete from CORELOGS_SOM where processid in(&&pid);
+delete from CORELOGS_SVM where processid in(&&pid);
+delete from COREPARMS_GA where processid in(&&pid);
+delete from COREPARMS_NN where processid in(&&pid);
+delete from COREPARMS_SOM where processid in(&&pid);
+delete from COREPARMS_SVM where processid in(&&pid);
+delete from MYLOG_MSE where processid in(&&pid);
+delete from MYLOG_RUN where processid in(&&pid);
+delete from ENGINETHREADS where processid in(&&pid);
+delete from DATAPARMS where processid in(&&pid);
+delete from ENGINEPARMS where processid in(&&pid);
+delete from CLIENTINFO where processid in(&&pid);
 //commit;
 
