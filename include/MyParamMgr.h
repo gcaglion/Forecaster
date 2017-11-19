28d46849f3d233321d908fddcb1c14de9a7adfcd
gcaglion <gcaglion@GCAGLION-ASUS>
2017-11-19 09:57:56 +0100

    incorporated MyEnums into MyParamMgr

--- include/MyParamMgr.h
+++ include/MyParamMgr.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <MyUtils.h>
-#include <MyEnums.h>
+#include <DataShape.h>
+#include <FXData.h>
+#include <FileData.h>
+#include <Tester.h>
+#include <MyEngines.h>
+#include <MyNN.h>
+#include <TSF.h>
+#include <MySOM.h>
+#include <MySVM.h>
 
 typedef struct sParamMgr {
 	//-- ini file and command-line overrides
@@ -12,12 +20,30 @@ typedef struct sParamMgr {
 
 
 
+
+#define MAXPARAMDESCLEN 20	// Max length for comma-separated lists of descriptions
+#define ARRAY_PARAMETER_MAX_LEN	100
+#define enumlist true
+
+//-- Forecaster Actions
+#define TRAIN_SAVE_RUN 0
+#define ADD_SAMPLES 1
+#define JUST_RUN 2
+
+//-- DataFile field separators
+#define COMMA ','
+#define TAB '\t'
+#define SPACE ' '
+
 #undef EXPORT
 #define EXPORT __declspec(dllexport)
 
 //-- command-line parameters processing
 int CLFail(int p);
 EXPORT int	CLProcess(int argc, char* argv[], tParamMgr* iniParms);
+
+//-- enumerator-type values decoder
+EXPORT int getEnumVal(char* edesc, char* eVal, int* oVal);
 
 //-- single value (int, double, char*, enum)
 EXPORT int ReadParamFromFile(char* pFileName, char* pParamName, int* oParamValue);
@@ -36,3 +62,4 @@ EXPORT int getParam(tParamMgr* iniParms, char* paramName, int** oparamVal);
 EXPORT int getParam(tParamMgr* iniParms, char* paramName, double** oparamVal);
 EXPORT int getParam(tParamMgr* iniParms, char* paramName, char*** oparamVal);
 EXPORT int getParam(tParamMgr* iniParms, char* paramName, int** oparamVal, bool isenum);
+
